42ea8439eaf77be6a976491c2d3f517bfedb01e4
James Thomas <jamesthomasdev2001@gmail.com>
2024-12-18 03:14:33 -0800

    Add router function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import entrypoints from "./global/entrypoints";
+import React from 'react';
+import { Route, Routes } from 'react-router';
 
+import HomePage from './pages/home/home';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 function App() {
+  //========================================================
+  const [routes, setRoutes] = useState(null);
+  useEffect(() => {
+    fetch(entrypoints.routes, {
+      method: "GET",
+    }).then((res) => res.json())
+      .then((res) => setRoutes(res))
+      .catch((err) => console.log(err));
+  }, []);
+  //=======================================================
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" component={HomePage} />
+        {routes &&
+          routes.map((route) => {
+            const ContentComponent =
+              React.lazy(() => import('./pages/' + route.component));
+            return (
+              <Route key={route.headerTitle}
+                path={route.path}
+                component={() => (
+                  <ContentComponent header={route.headerTitle} />
+                )}
+              />);
+          })}
+      </Routes>
+      <Footer>
+        Copyright © 2015-{new Date().getFullYear()} Inc.
+        <br />
+        This software is reserved Software Copyright.
+      </Footer>
     </div>
   );
 }
