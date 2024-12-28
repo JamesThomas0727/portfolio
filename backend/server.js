8c0739c423959da30302014f3a536898aae83d87
James Thomas <jamesthomasdev2001@gmail.com>
2024-12-27 20:19:02 -0800

    Update .env file and add node module in backend

--- backend/server.js
+++ backend/server.js
@@ -7,59 +7,36 @@ const host = process.env.HOST;
 const port = process.env.PORT;
 const db_uri = process.env.DB_URI;
 
+const SkillModel = require("./models/Skill");
 const ProjectModel = require("./models/Project");
+const EducationModel = require("./models/Education");
+// const EducationModel = require("./models/Education");
 
-
+////////////////////////////////////////////////////////
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(DB_URI)
+mongoose.connect(db_uri)
     .then(() => console.log('MongoDB connected successfully!'))
     .catch((err) => console.log(err));
+
+/////////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/faqs', async (req, res) => {
+app.get('/projects', async (req, res) => {
     try {
-        const faqs = await FAQModel.find();
-        res.json(faqs);
+        const projects = await ProjectModel.find();
+        res.json(projects);
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
-app.get('/products', async (req, res) => {
-    let brand = req.query.brand;
-    console.log(brand);
-    if (brand !== undefined) {
-        try {
-            let products = await ProductModel.find()
-                .where('brand', brand);
-            res.json(products);
-        } catch (err) {
-            res.status(500).send(err.message);
-        }
-    }
-    else {
-        try {
-            let products = await ProductModel.find()
-            res.json(products);
-        } catch (err) {
-            res.status(500).send(err.message);
-        }
-    }
-});
-app.get('/currencies', async (req, res) => {
-    try {
-        const currency_rates = await CurrencyRateModel.find();
-        res.json({currency: currency_rates});
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
+
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server running at ${host}:${port}`)
 });
